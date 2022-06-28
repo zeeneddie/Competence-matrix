5bd4a4c9028314205ee21f77db2e502eac3da41e
TovDmitrij <dmxikka@gmail.com>
2022-06-28 15:52:29 +0300

    Реализовал модели компетенций, индикаторов и знаний

--- Competence_Matrix/main/models.py
+++ Competence_Matrix/main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import json
 
-# Create your models here.
+#--------------------------------------------------------
+# Компетенции, индикаторы и ЗУВ (Знания, умения, владения)
+#--------------------------------------------------------
+class Model:
+    def __init__(self, type, name, description, children):
+        self.type = type
+        self.name = name
+        self.description = description
+        self.children = children
+    def ChangeModel(self, newName, newDescription):
+        self.name = newName
+        self.description = newDescription
+
+def loadJSON():
+    with open('DB.json', 'r') as file:
+        data = json.load(file)
+    return data
+def saveJSON(data):
+    with open('DB.json', 'w') as file:
+        json.dump(data, file, indent=3)
--- Competence_Matrix/main/views.py
+++ Competence_Matrix/main/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-import json
 
+#Загрузка основной страницы
 def index(request):
     return render(request, 'main/index.html')
-
-def loadJSON():
-    with open('DB.json', 'r') as file:
-        data = json.load(file)
-    return data
-
-def saveJSON(data):
-    with open('DB.json', 'w') as file:
-        json.dump(data, file, indent=3)
