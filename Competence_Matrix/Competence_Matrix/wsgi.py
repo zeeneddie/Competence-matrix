d98e1a1a87b5994830dddf2a3d86955a36f3a745
TovDmitrij <dmxikka@gmail.com>
2022-06-28 15:23:04 +0300

    Реализовал возможность сериализовать и де- файлы с типом JSON

--- Competence_Matrix/Competence_Matrix/wsgi.py
+++ Competence_Matrix/Competence_Matrix/wsgi.py
@@ -1,16 +1,5 @@
-"""
-WSGI config for Competence_Matrix project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Competence_Matrix.settings')
-
 application = get_wsgi_application()
